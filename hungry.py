bd0c78702f73a38902c41a42d2735342d763d76f
unknown <kaushikdayabar@mail.com>
2024-12-26 23:29:40 +0530

    Removed pizza, burger and codebasics

--- hungry.py
+++ hungry.py
@@ -2,9 +2,7 @@ hungry=input("Are You Hungry?")
 
 if hungry=="yes":
     print("eat samosa")
-    print("pizza23")
-    print("burger")
-    print("code basics")
+
 else:
     print("Do Homework")
 
