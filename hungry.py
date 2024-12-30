b2f89aa4ad403966934d5cfe1886a6d4b5301d73
unknown <kaushikdayabar@mail.com>
2024-12-31 01:31:19 +0530

    restored

--- hungry.py
+++ hungry.py
@@ -1,7 +1,9 @@
 hungry=input("Are You Hungry?")
 
 if hungry=="yes":
-    print("eat fries")
+    print("eat samosa")
+    print("eat pizza")
+    print("eat burger")
 else:
     print("Do Homework")
 
